378a0536ed21e48822bcf008d66021c58d043efa
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-05-09 20:11:45 -0300

    feat: Adicionando express server.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('OLÁ MUNDÃO')
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 7474;
+
+app.listen(port, () => console.log(`Subiu na porta ${port}!`));
