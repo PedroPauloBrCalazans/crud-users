79cbe9b5133493a17aa56bd7efdc183832bbef96
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-05-15 22:22:53 -0300

    feat: concluído rota de create.

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,15 @@ import { config } from "dotenv";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { GetUsersController } from "./controller/get-users/get-users";
 import { MongoClient } from "./database/mongo";
+import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user";
+import { CreateUserController } from "./controller/create-user/create-user";
 
 const main = async () => {
   config();
+
   const app = express();
+
+  app.use(express.json());
 
   await MongoClient.connect();
 
@@ -16,6 +21,20 @@ const main = async () => {
     const getUsersController = new GetUsersController(mongoGetUsersRepository);
 
     const { body, statusCode } = await getUsersController.handle();
+
+    res.send(body).status(statusCode);
+  });
+
+  app.post("/users", async (req, res) => {
+    const mongoCreateUserRepository = new MongoCreateUserRepository();
+
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository
+    );
+
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
 
     res.send(body).status(statusCode);
   });
