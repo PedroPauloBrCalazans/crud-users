1142731326ed2c71739167769efeb89f69ca244a
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-06-20 20:25:05 -0300

    feat: Add MongoUpdateUserRepository.

---
+++ src/controller/update-user/protocols.ts
@@ -0,0 +1,11 @@
+import { User } from "../../models/user";
+
+export interface UpdateUserParams {
+  nome?: string;
+  sobrenome?: string;
+  email?: string;
+}
+
+export interface IUpdateUserRepository {
+  updateUser(id: string, params: UpdateUserParams): Promise<User>;
+}
