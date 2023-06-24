c8d489179d571ccb5a381693c38bf7b5f114211d
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-06-24 14:08:24 -0300

    refactor: add controller helper function.

--- src/controller/create-user/create-user.ts
+++ src/controller/create-user/create-user.ts
@@ -1,4 +1,5 @@
 import { User } from "../../models/user";
+import { badRequest, created, serverError } from "../helpers";
 import { HttpRequest, HttpResponse, IController } from "../protocols";
 import { CreateUserParams, ICreateUserRepository } from "./protocols";
 
@@ -9,40 +10,28 @@ export class CreateUserController implements IController {
 
   async handle(
     httpRequest: HttpRequest<CreateUserParams>
-  ): Promise<HttpResponse<User>> {
+  ): Promise<HttpResponse<User | string>> {
     try {
       const camposObrigatorios = ["nome", "sobrenome", "email", "password"];
 
       for (const campos of camposObrigatorios) {
         if (!httpRequest?.body?.[campos as keyof CreateUserParams]?.length) {
-          return {
-            statusCode: 400,
-            body: `Campo(s) ${campos} é obrigatório(s).`,
-          };
+          return badRequest(`Campo(s) ${campos} é obrigatório(s).`);
         }
       }
       const emailValido = validator.isEmail(httpRequest.body!.email);
 
       if (!emailValido) {
-        return {
-          statusCode: 400,
-          body: "E-mail é inválido",
-        };
+        return badRequest("E-mail é inválido");
       }
 
       const user = await this.createUserRepository.createUser(
         httpRequest.body!
       );
 
-      return {
-        statusCode: 201,
-        body: user,
-      };
+      return created<User>(user);
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: "Algo deu errado.",
-      };
+      return serverError();
     }
   }
 }
